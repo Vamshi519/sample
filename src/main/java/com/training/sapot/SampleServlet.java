b8c9f6e39d98046f4dcd4d0983b7661a0c1443c1
Rakesh_k <krakeshk17@gmail.com>
2017-02-12 10:27:57 -0500

    comments addressed

--- src/main/java/com/training/sapot/SampleServlet.java
+++ src/main/java/com/training/sapot/SampleServlet.java
@@ -4,7 +4,7 @@ public class SampleServlet {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Inside sample project and using git creating branch new master");
+		System.out.println("Inside sample project");
 	}
 
 }
